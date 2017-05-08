9017c5ababdad98791f12430286cd30f52556321
Manoj Jeswani <manojjeswanig@gmail.com>
2017-05-09 01:42:28 +0530

    Made it more generalised
    updated for universal use

--- rename_automate.py
+++ rename_automate.py
@@ -1,14 +1,14 @@
 import os
-main_path="/media/mj/F6A400C5A4008A77/spt/"
+main_path="PATH OF DIRECTORY CONTAINING FILES TO BE RENAMED"
 source_dir_name=""
 os.chdir(main_path+source_dir_name)
 bn=os.popen("ls").read().split("\n")
-k=1
+k=1 #name would start from 1 
 for i in bn:
 	print(i)
 	try:
 
-		os.rename(i,str(k)+".jpg")
+		os.rename(i,str(k)+".jpg") #extension here can be changed according to the type of files being renamed
 		print("SUCCESSFULLY RENAMED")
 	except:
 		print("Rename unsuccessful")
